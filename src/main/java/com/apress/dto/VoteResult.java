e6c643b77d3cd13c43008ab6d8b547fae65bde13
Wajid Poernomo <Wajid.Poernomo@monash.edu>
2017-10-19 16:56:48 +1100

    first commit

---
+++ src/main/java/com/apress/dto/VoteResult.java
@@ -0,0 +1,20 @@
+package com.apress.dto;
+import java.util.Collection;
+
+public class VoteResult {
+        private int totalVotes;
+        private Collection<OptionCount> results;
+        // Getters and Setters omitted for brevity
+		public int getTotalVotes() {
+			return totalVotes;
+		}
+		public void setTotalVotes(int totalVotes) {
+			this.totalVotes = totalVotes;
+		}
+		public Collection<OptionCount> getResults() {
+			return results;
+		}
+		public void setResults(Collection<OptionCount> results) {
+			this.results = results;
+		}
+}
